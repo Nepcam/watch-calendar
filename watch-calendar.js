69ed76804db9fb75d2fd8fe401a91b0e8f074fdc
nepcam <cameron.nepe@gmail.com>
2018-07-24 11:58:18 +1200

    add time day and date

--- watch-calendar.js
+++ watch-calendar.js
@@ -17,4 +17,41 @@ function showWatch() {
     var showTime = document.getElementById("Time");
     var showCalender = document.getElementById("Calender");
     
+    var $Minutes = String(Minutes);
+    while($Minutes.length <= 1) {
+        $Minutes = 0 + $Minutes;
+    }
     
+    if(Hours > 12) {
+        Hours -= 12;
+        showTime.innerHTML = 
+        Hours + ": " + $Minutes + " pm" + " (" + Seconds + " secs)";
+    }else if(Hours == 12) {
+        showTime.innerHTML = 
+        Hours + ": " + $Minutes + " noon" + " (" + Seconds + " secs)";
+    }else {
+        showTime.innerHTML = 
+        Hours + ": " + $Minutes + " am" + " (" + Seconds + " secs)";
+    }
+    
+    switch(Day) {
+        case 0 : showDay = "Sunday";
+        break;
+        case 1 : showDay = "Monday";
+        break;
+        case 2 : showDay = "Tuesday";
+        break;
+        case 3 : showDay = "Wednesday";
+        break;
+        case 4 : showDay = "Thursday";
+        break;
+        case 5 : showDay = "Friday";
+        break;
+        case 6 : showDay = "Saturday";
+        break;
+    }
+    
+    showCalender.innerHTML = todayDate + "/ " + (Month+1) + "/ " + Year + " ( " + showDay + " )";
+    
+}
+
